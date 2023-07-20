a8a0baca31e892c42fd2cd9112b115c40d33a5d4
Alona <mykyshka2013@gmail.com>
2023-07-20 11:11:52 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  const bodyEl = document.querySelector('body')
+  const startBtn = document.querySelector('button[data-start]');
+  const stopBtn = document.querySelector('button[data-stop]');
+  
+  function changeColor(){
+    const backGroundColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = backGroundColor;
+  }
+  
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+        timerId = setInterval(changeColor, 1000);
+        stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+clearInterval(timerId)
+stopBtn.disabled = true;
+startBtn.disabled = false;
+});
 
