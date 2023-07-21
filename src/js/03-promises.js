0185b207f4dfdb74367c38cc042b7ce66c83be12
Alona <mykyshka2013@gmail.com>
2023-07-21 13:30:09 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import Notiflix from 'notiflix';
+import 'notiflix/src/notiflix.css';
+
+ const formEl = document.querySelector('.form');
+ formEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+  const { delay, step, amount } = evt.target.elements;
+  console.log(delay.value, step.value, amount.value);
+  let currentDelay = Number(delay.value);
+  for(let i = 1; i <= Number(amount.value); i +=1){
+     createPromise(i, currentDelay)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+      currentDelay += Number(step.value)
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+       reject({ position, delay }) 
+      }
+    }, delay)
+  })
+  return promise;
 }
